d33c78932254854a31e547b71100c7c18013bac3
유지연 <jiyeonyou0416@naver.com>
2022-11-01 10:54:08 +0900

    first

---
+++ src/test/java/com/ssafy/ky/WhereIsMyHome87SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereIsMyHome87SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
